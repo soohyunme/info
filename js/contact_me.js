000e136be4bb75356a7c2cfc3e28a24620dbda92
yopbi <50914564+yopbi@users.noreply.github.com>
2020-02-25 17:39:43 +0900

    Add files via upload
    20.02.25
    index 화면 타이틀 변경 및 초기 화면 재구성
    contact 폼 전송 시 formspree를 이용하여 폼을 전송하는데 폼 전송 시 사이트가 이동되는데  cors 정책으로 인해 오류가 발생,
    하지만 폼 전송과 fromspree는 정상적으로 작동하므로 안내 문구만 수정

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,6 +12,7 @@ $(function() {
       var email = $("input#email").val();
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
+      var flag = false;
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
@@ -20,7 +21,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://formspree.io/mdogpore",
+        url: "https://formspree.io/mjvrgpyp",
         type: "POST",
         data: {
           name: name,
@@ -30,6 +31,7 @@ $(function() {
         },
         cache: false,
         success: function() {
+          flag=true;
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -42,16 +44,20 @@ $(function() {
           $('#contactForm').trigger("reset");
         },
         error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
         },
         complete: function() {
+          if(flag==false){
+              // Success message
+              $('#success').html("<div class='alert alert-success'>");
+              $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+              $('#success > .alert-success')
+                .append("<strong>Your message has been sent.<BR> If a server error occurs, your message may not be delivered. </strong>");
+              $('#success > .alert-success')
+                .append('</div>');
+              //clear all fields
+              $('#contactForm').trigger("reset");
+          }
           setTimeout(function() {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
           }, 1000);
